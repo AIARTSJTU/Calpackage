64372c04f7ae5e82ad7fae052015907b6334e5a5
844294823@qq.com <844294823@qq.com>
2020-11-07 19:31:39 +0800

    Update calpackage.py

--- calpackage.py
+++ calpackage.py
@@ -1,8 +1,8 @@
-import pytorch_ssim
+from Calpackage import pytorch_ssim
 import torch
 from torch.autograd import Variable
-from imageReader import *
-from psnr import *
+#from imageReader import *
+from Calpackage.psnr import *
 import lpips
 
 
@@ -14,22 +14,29 @@ class calpackage(object):
         self.npix = width * height
 
     def call(self, img1, img2):
+        #psnr part
+        psnr = psnr_cal((img1.numpy()+1.)/2.,(img2.numpy()+1.)/2.)
+
         #lpips part
         loss_fn_alex = lpips.LPIPS(net='alex') # best forward scores
         loss_fn_vgg = lpips.LPIPS(net='vgg') # closer to "traditional" perceptual loss, when used for optimization
+        if torch.cuda.is_available():
+            loss_fn_alex = loss_fn_alex.cuda()
+            loss_fn_vgg = loss_fn_vgg.cuda()
+            img1 = img1.cuda()
+            img2 = img2.cuda()
         lpips_value_alex = loss_fn_alex(img1, img2)
         lpips_value_vgg = loss_fn_vgg(img1, img2)
 
         # img1_01 = (img1.numpy()+1.)/2.
         # img2_01 = (img2.numpy()+1.)/2.
 
-        #psnr part
-        psnr = psnr_cal((img1.numpy()+1.)/2.,(img2.numpy()+1.)/2.)
+
 
         #ssim part
-        if torch.cuda.is_available():
-            img1 = img1.cuda()
-            img2 = img2.cuda()
+        #if torch.cuda.is_available():
+        #    img1 = img1.cuda()
+        #    img2 = img2.cuda()
 
         #print(pytorch_ssim.ssim(img1, img2))
 
@@ -37,4 +44,3 @@ class calpackage(object):
         ssim_value = ssim_loss(img1, img2)
 
         return lpips_value_alex,lpips_value_vgg, psnr, ssim_value
-
